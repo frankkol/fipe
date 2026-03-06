834fa5707bbec2e81f075e6b070908835b77ae9f
Frank Rodrigues Oliveira - PrestServ <frank.oliveira.prestserv@petrobras.com.br>
2026-03-06 18:50:59 -0300

    Changes package and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Importação correta
 
 export default defineConfig({
+  base: '/fipe/',
   plugins: [
     react(),
     tailwindcss(), // Certifique-se dos parênteses ()
