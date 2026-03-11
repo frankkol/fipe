4b8c8d0dd131a62aaf08b55f3e0a60e26244369a
Frank Rodrigues Oliveira - PrestServ <frank.oliveira.prestserv@petrobras.com.br>
2026-03-11 15:00:35 -0300

    Add function top/down

--- src/components/Fipe.tsx
+++ src/components/Fipe.tsx
@@ -16,9 +16,34 @@ const Fipe = () => {
     const headStyleActive: string = "bg-white border-b-2 border-blue-500 text-blue-600";
     const headStyleDisable: string = "text-stone-500 hover:text-stone-700 hover:bg-stone-100";
 
+    const [isGoingUp, setIsGoingUp] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
     useEffect(() => {
         if (automobile === undefined) setTable([]);
     }, [automobile])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollHeight = document.documentElement.scrollHeight;
+            const clientHeight = document.documentElement.clientHeight;
+            const scrollTotal = scrollHeight - clientHeight;
+            setIsVisible(scrollTotal > 50);
+            setIsGoingUp(window.scrollY > (scrollTotal / 2));
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [automobile, table]);
+
+    const handleScrollAction = () => {
+        if (isGoingUp) {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else {
+            window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
+        }
+    };
 
     return (
         <div className='bg-stone-100 min-h-screen w-full flex flex-col items-center p-4 sm:justify-center'>
@@ -46,6 +71,15 @@ const Fipe = () => {
                             <Graphy data={table} />
                             <TableFipe data={table} />
                         </div>
+                    )}
+                    {isVisible && (
+                        <button
+                            onClick={handleScrollAction}
+                            className={`fixed bottom-6 right-4 w-14 h-14 bg-gray-200 text-slate-700 rounded-full shadow-2xl flex items-center justify-center transition-all duration-500 z-50 md:hidden ${isGoingUp ? "rotate-180" : "rotate-0"
+                                }`}
+                        >
+                            <span className="text-3xl mb-1">↓</span>
+                        </button>
                     )}
                 </div>
             </div>
